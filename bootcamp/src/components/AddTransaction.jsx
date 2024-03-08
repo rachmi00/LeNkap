cb6763d0d7575ddd0c9e3cfae810fc6c234ae355
rachmi00 <r.agyingi@gmail.com>
2024-03-08 23:00:15 +0100

    the end

--- bootcamp/src/components/AddTransaction.jsx
+++ bootcamp/src/components/AddTransaction.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext , useEffect} from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { HomeIcon } from "@heroicons/react/solid";
 import { GlobalContext } from "../context/GlobalState";
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function AddTransaction() {
-
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
   const [categoryId, setCategoryId] = useState("");
@@ -24,10 +23,9 @@ function AddTransaction() {
       { id: 1, name: "Food" },
       { id: 2, name: "Transportation" },
       { id: 3, name: "Utilities" },
-      {id:33, name: "Clothes"},
+      { id: 33, name: "Clothes" },
     ]);
   }, []);
-
 
   const onSubmitCategory = (e) => {
     e.preventDefault();
@@ -47,22 +45,19 @@ function AddTransaction() {
     const formDatas = { name, amount, categoryId, type };
     const authToken = localStorage.getItem('token');
 
-    
     try {
-      const response = await axios.post('https://le-nkap-v1.onrender.com/transactions', formDatas, 
+      const response = await axios.post('https://le-nkap-v1.onrender.com/transactions', formDatas,
         {
           headers: {
             Authorization: `Bearer ${authToken}`, // Assuming you store the token in localStorage
-          }},);
-      
-      
+          }
+        });
 
       console.log(response.data);
     }
     catch (error) {
       console.error(error)
     }
-    
 
     const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
@@ -111,103 +106,103 @@ function AddTransaction() {
               </div>
             </div>
           )}
-           <div className="w-full sm:max-w-md bg-white p-6 rounded-md shadow-md mt-8">
-            <h3 className="text-lg font-semibold mb-4">Add new category</h3>
-            <form onSubmit={onSubmitCategory} className="space-y-4">
-              <div>
-                <label
-                  htmlFor="categoryName"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Category name
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-3xl">
+            <div className="bg-white p-6 rounded-md shadow-md">
+              <h3 className="text-lg font-semibold mb-4">Add new category</h3>
+              <form onSubmit={onSubmitCategory} className="space-y-4">
+                <div>
+                  <label
+                    htmlFor="categoryName"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Category name
                 </label>
-                <input
-                  type="text"
-                  value={categoryName}
-                  onChange={(e) => setCategoryName(e.target.value)}
-                  placeholder="Enter category name..."
-                  className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              >
-                Add Category
+                  <input
+                    type="text"
+                    value={categoryName}
+                    onChange={(e) => setCategoryName(e.target.value)}
+                    placeholder="Enter category name..."
+                    className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                  Add Category
               </button>
-            </form>
-          </div>
-          <div className="w-full sm:max-w-md bg-white p-6 rounded-md shadow-md">
-            <h3 className="text-lg font-semibold mb-4">Add new transaction</h3>
-            <form onSubmit={onSubmitTransaction} className="space-y-4">
-              <div>
-                <label
-                  htmlFor="text"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Transaction name
+              </form>
+            </div>
+            <div className="bg-white p-6 rounded-md shadow-md">
+              <h3 className="text-lg font-semibold mb-4">Add new transaction</h3>
+              <form onSubmit={onSubmitTransaction} className="space-y-4">
+                <div>
+                  <label
+                    htmlFor="text"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Transaction name
                 </label>
-                <input
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Enter text..."
-                  className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                />
-              </div>
-              <div>
-                <label htmlFor="type" className="block text-md font-mediun text-gray-700"> Type</label>
-              <input type="text" name="type" value={type}  onChange={(e)=>setType(e.target.value)} placeholder="income or expense?"
-                 className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
-              <div>
-                <label
-                  htmlFor="amount"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Amount
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Enter text..."
+                    className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="type" className="block text-md font-mediun text-gray-700"> Type</label>
+                  <input type="text" name="type" value={type} onChange={(e) => setType(e.target.value)} placeholder="income or expense?"
+                    className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div>
+                <div>
+                  <label
+                    htmlFor="amount"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Amount
                   <span className="text-gray-500 text-xs ml-1">
-                    (negative - expense, positive - income)
+                      (negative - expense, positive - income)
                   </span>
+                  </label>
+                  <input
+                    type="number"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    placeholder="Enter amount..."
+                    className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="category"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Category
                 </label>
-                <input
-                  type="number"
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                  placeholder="Enter amount..."
-                  className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="category"
-                  className="block text-sm font-medium text-gray-700"
+                  <select
+                    value={categoryId}
+                    onChange={(e) => setCategoryId(e.target.value)}
+                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
+                  >
+                    <option value="">Select category...</option>
+                    {categories.map((category) => (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 >
-                  Category
-                </label>
-                <select
-                  value={categoryId}
-                  onChange={(e) => setCategoryId(e.target.value)}
-                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
-                >
-                  <option value="">Select category...</option>
-                  {categories.map((category) => (
-                    <option key={category.id} value={category.id}>
-                      {category.name}
-                    </option>
-                  ))}
-                </select>
-
-              </div>
-              <button
-                type="submit"
-                className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              >
-                Add Transaction
+                  Add Transaction
               </button>
-            </form>
+              </form>
+            </div>
           </div>
-         
         </div>
       </div>
     </main>
