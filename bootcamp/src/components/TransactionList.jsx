437236ba1b2c6409d9f3c7422050f51ca31fd9c7
Rachel Mifor Agyingi <119732500+rachmi00@users.noreply.github.com>
2025-02-25 23:59:57 +0100

    Update TransactionList.jsx

--- bootcamp/src/components/TransactionList.jsx
+++ bootcamp/src/components/TransactionList.jsx
@@ -1,20 +1,20 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import Transaction from "./Transaction";
 
-function TransactionList(){
-    const {transactions} = useContext(GlobalContext)
-    
-    
+function TransactionList() {
+  const { transactions } = useContext(GlobalContext);
 
-    return(
-       <div>
-         <h3 className="text-2xl font-bold text-blue-400 mb-4 mx-4 mt-4">History</h3>
-        <ul >
-            {transactions.map(transaction =>(<Transaction key={transaction.id} transaction={transaction}/>))}
-            
-        </ul>
-       </div>
-    )
-    }
-export default TransactionList
+  return (
+    <div>
+      <h3 className="text-2xl font-bold text-blue-700 mb-4">History</h3>
+      <ul className="space-y-4">
+        {transactions.map(transaction => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TransactionList;
